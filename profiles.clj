5a6720ddec8033ebcdc8ea11aa6a28a65dcb7a43
Nishant Maniam <nishant.maniam@gmail.com>
2016-07-26 12:53:45 -0400

    Delete profiles.clj

--- profiles.clj
+++
@@ -1,7 +0,0 @@
-{:dev
- {:env
-  {:app-consumer-key "Wg2yrTAh5SWcPu29s4X4xlJcI"
-   :app-consumer-secret "Rv3w1amelkc15trKoJmcQoG2JWLHrCCEJ182skd1z8BoPxjROW"
-   :user-access-token "757675980248973312-JpqRMMnTkx6NDg6t44Rt8BjAZmsmuUM"
-   :user-access-secret "zur1fCcbTdVxkdbQzKJXzhUSaIXvZxmcrQ9XgNmYDUrrx"
-   }}}
